f82215db5389534168e073ddc5995cfa3bb8b557
ThraxHamilton <mmcclenton16@gmail.com>
2018-10-05 15:29:04 -0500

    finished

--- Program.cs
+++ Program.cs
@@ -1,53 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace employeeclasses {
-
+namespace classes {
+    // Create public class
     public class Company {
         public string Name { get; set; }
         public DateTime CreatedOn { get; set; }
-        public List<string> employees = new List<string> ();
-        public void Introduce (string to);
+        public List<Employees> employeeList = new List<Employees> ();
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
 
-    };
+            The constructor will set the value of the public properties
+        */
+        public Company (string CompanyName, DateTime CompanyCreatedOn) {
+            Name = CompanyName;
+            CreatedOn = CompanyCreatedOn;
+        }
+
+        public void ListEmployees () {
+
+            foreach (Employees employee in employeeList) {
+                Console.WriteLine ($"{employee.FirstName} " + $"{ employee.LastName} works for" + $" {this.Name}, As" + $"{employee.EmployeeTitle} since " + $"{employee.StartedWorking}");
+            }
+        }
+    }
 
     public class Employees {
-        public string FirstName { set; get; }
-        public string LastName { set; get; }
-        public string EmployeeTitle { set; get; }
-        public DateTime StartedWorking { get;  }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmployeeTitle { get; set; }
+        public DateTime StartedWorking { get; set; }
 
     }
 
     class Program {
         static void Main (string[] args) {
             // Ref to Company class. Now you can give it info
-            Company Conglomo = new Company () {
-                Name = "Foundations Inc",
-                CreatedOn = DateTime.Now
-            };
+            Company Conglomo = new Company ("Conglomo", DateTime.Now);
             // Ref to Employees. Populate with names and info
-            List<Employees> employees = new List<Employees> () {
-                // Create three employees
-                new Employees () {
+            // Create three employees
+            Employees Dominic = new Employees () {
                 FirstName = "Dominic",
                 LastName = "Kennedy",
-                EmployeeTitle = "IT Security Officer",
+                EmployeeTitle = "OPM",
                 StartedWorking = DateTime.Now
-                },
-                new Employees () {
+            };
+            Employees Bob = new Employees () {
                 FirstName = "Bob",
                 LastName = "Saggett",
                 EmployeeTitle = "Human Resource Manager",
                 StartedWorking = DateTime.Now
-                },
-                new Employees () {
+            };
+            Employees Earnest = new Employees () {
                 FirstName = "Earnest",
                 LastName = "Hemmingway",
-                EmployeeTitle = "CTO",
+                EmployeeTitle = "IT Security Officer",
                 StartedWorking = DateTime.Now
-                }
             };
+            Conglomo.employeeList.Add (Dominic);
+            Conglomo.employeeList.Add (Bob);
+            Conglomo.employeeList.Add (Earnest);
+
+            Conglomo.ListEmployees();
         }
+
     }
 }
